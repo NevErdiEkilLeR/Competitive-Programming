75403e0aeac9e26512e18ab5189011d40ff7885c
cupjoo <cupjoo@naver.com>
2020-02-25 19:18:44 +0900

    74일 차

--- Junyoung/기본유형/구간_합_구하기.java
+++ Junyoung/기본유형/구간_합_구하기.java
@@ -3,9 +3,34 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        new Solution();
+class SegTree {
+    private long[] tree;    // root node : 1
+
+    public SegTree(long[] num){
+        int h = (int)Math.ceil(Math.log(num.length)/Math.log(2));    // log2(N)
+        tree = new long[1 << (h+1)];
+        init(num, 1, 0, num.length-1);
+    }
+    public long init(long[] num, int node, int s, int e){
+        if(s == e)  return tree[node] = num[s];
+        int m = (s+e)/2;
+        return tree[node] = init(num, node*2, s, m)
+                + init(num, node*2+1, m+1, e);
+    }
+    // [node's range]: s~e & [sum range]: sumL~sumR
+    public long sum(int node, int s, int e, int sumL, int sumR){
+        if(e < sumL || sumR < s)    return 0;
+        if(sumL <= s && e <= sumR)  return tree[node];
+        int m = (s+e)/2;
+        return sum(node*2, s, m, sumL, sumR)
+                + sum(node*2+1, m+1, e, sumL, sumR);
+    }
+    public long update(int node, int s, int e, int idx, long val){
+        if(idx < s || idx > e)  return tree[node];
+        if(s == e)  return tree[node] = val;
+        int m = (s+e)/2;
+        return tree[node] = update(node*2, s, m, idx, val)
+                + update(node*2+1, m+1, e, idx, val);
     }
 }
 
@@ -48,32 +73,8 @@ class Solution {
     }
 }
 
-class SegTree {
-    private long[] tree;    // root node : 1
-
-    public SegTree(int N){
-        int h = (int)Math.ceil(Math.log(N)/Math.log(2));
-        tree = new long[1 << (h+1)];
-    }
-    public long init(long[] num, int node, int s, int e){
-        if(s == e)  return tree[node] = num[s];
-        int m = (s+e)/2;
-        return tree[node] = init(num, node*2, s, m)
-                + init(num, node*2+1, m+1, e);
-    }
-    // node's range: s~e & sum range: sumL~sumR
-    public long sum(int node, int s, int e, int sumL, int sumR){
-        if(e < sumL || sumR < s)    return 0;
-        if(sumL <= s && e <= sumR)  return tree[node];
-        int m = (s+e)/2;
-        return sum(node*2, s, m, sumL, sumR)
-                + sum(node*2+1, m+1, e, sumL, sumR);
-    }
-    public long update(int node, int s, int e, int idx, long diff){
-        if(idx < s || idx > e)  return tree[node];
-        if(s == e)  return tree[node] = diff;
-        int m = (s+e)/2;
-        return tree[node] = update(node*2, s, m, idx, diff)
-                + update(node*2+1, m+1, e, idx, diff);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Solution();
     }
 }
